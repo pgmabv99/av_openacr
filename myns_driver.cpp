abb209eef018555229ce5ce1cbb6a523815c68ea
alexey vorovich <pgmabv99@gmail.com>
2024-11-05 20:03:33 -0500

    swtich other obj_alloc stidy smallstr50 constructor

--- myns_driver.cpp
+++ myns_driver.cpp
@@ -21,44 +21,106 @@
 #include "include/algo.h"
 #include "include/algo.h"
 #include "include/myns.h"
+#include <string>
 
-void myns::Main()
+namespace myns
 {
-    prlog("manual creation of orders");
 
-    myns::Orders &order14 = myns::ind_orders_GetOrCreate("order14");
-    order14.amt = 14;
-    prlog("order inserted in memory " << order14.orders << " amt " << order14.amt);
-    myns::Orders &order16  = myns::ind_orders_GetOrCreate("order16");
-    order16.amt = 16;
-    prlog("order inserted in memory " << order16.orders << " amt " << order16.amt);
-    prlog("==delete order16");
-    orders_Delete(order16);
-    
+    typedef struct
+    {
+        // std::string name;
+        // algo::Smallstr50 name;
+        const char *name;
+        int amt;
+        myns::Orders *order;
+    } order_data_t;
+    typedef struct
+    {
+        order_data_t *order_data;
+    } mcb_t;
+    void scan();
+    void load_data(mcb_t* mcb);
+}
+
+void myns::scan()
+{
     prlog("==scan  ");
     ind_beg(myns::_db_zd_orders_curs, order, myns::_db)
     {
-        prlog(Keyval("order", order.orders));
-        prlog(Keyval("amount", order.amt));
+        prlog(Keyval("order", order.orders)<<Keyval("amount", order.amt));
 
     }
     ind_end;
-    
-    prlog("==scan list and update ");
-        ind_beg(myns::_db_zd_orders_curs, order, myns::_db)
+}
+
+void myns::load_data(myns::mcb_t* mcb)
+{
+    myns::Orders *order = nullptr;
+    #define N_ORDERS 4
+    mcb->order_data = (myns::order_data_t *)calloc(N_ORDERS, sizeof(myns::order_data_t));
+    for (int i = 0; i < N_ORDERS; i++)
+    // {
+    //     mcb->order_data[i].order = nullptr;
+    //     // std::string order_name = "order" + std::to_string(i);
+    //     // mcb->order_data[i].name = order_name.c_str();
+    //     // mcb->order_data[i].name = algo::Smallstr50("order" + std::to_string(i));
+    //     mcb->order_data[i].name = algo::Smallstr50(std::string("order")) ;
+    //     mcb->order_data[i].amt = i*10;
+    // }
+    mcb->order_data[0] = {"order0", 0};
+    mcb->order_data[1] = {"order1", 10};
+    mcb->order_data[2] = {"order2", 20};
+    mcb->order_data[3] = {"order3", 30};
+
+    for (long unsigned int i = 0; i < N_ORDERS; i++)
     {
-        prlog(Keyval("order", order.orders));
-        prlog(Keyval("amount", order.amt));
-        if (order.amt < 15) {
-            prlog("deleting  order " << order.orders);
-            orders_Delete(order);
-        } else {
-            prlog("adding 1000 to order -> " << order.amt);
-            order.amt +=1000;
+        order = &orders_Alloc();
+        if (!orders_XrefMaybe(*order))
+        {
+            orders_Delete(*order);
+            order = NULL;
         }
-
+        else
+        {
+            order->orders = mcb->order_data[i].name;
+            order->amt = mcb->order_data[i].amt;
+            mcb->order_data[i].order = order;
+            prlog("order inserted in memory " << order->orders << " amt " << order->amt);
+        };
     }
-    ind_end;
+}
+void myns::Main()
+{
+    prlog("manual creation and deletion of orders");
+
+    myns::mcb_t *mcb = (myns::mcb_t *)calloc(1, sizeof(mcb_t));
+
+    myns::load_data(mcb);
+     
+    myns::scan();
+
+    auto  iorder=3-1;
+    prlog("==delete order "<<iorder);
+    prlog("==delete order name "<<mcb->order_data[iorder].name);
+    myns::Orders* order= mcb->order_data[iorder].order;
+    orders_Delete(*order);
+    
+    myns::scan();
+    
+    // prlog("==scan list and update ");
+    //     ind_beg(myns::_db_zd_orders_curs, order, myns::_db)
+    // {
+    //     prlog(Keyval("order", order.orders)<<Keyval("amount", order.amt));;
+    //     if (order.amt < 15) {
+    //         prlog("deleting  order " << order.orders);
+    //         orders_Delete(order);
+    //     } else {
+    //         prlog("adding 1000 to order -> " << order.amt);
+    //         order.amt +=1000;
+    //     }
+
+    // }
+    // ind_end;
 
     myns::MainLoop();
 }

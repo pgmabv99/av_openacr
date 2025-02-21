7625fac631478931d5b4f5ea075845a86a8c5aa8
alexey vorovich <alexey.vorovich@gmail.com>
2025-02-20 19:40:16 -0500

    more acr_ed fieldd

--- 525.sh
+++ 525.sh
@@ -12,16 +12,24 @@ cd $HOME/arnd
 acr_ed -del  -ctype x2bm_pcap.Ftcp_pair -write || true
 acr_ed -del  -ctype x2bm_pcap.Fframe     -write || true
 
-
-acr_ed -create -ctype x2bm_pcap.FTcp_pair -pooltype Tpool -indexed -write  -comment "tcp pair entry"
-acr_ed -create -field x2bm_pcap.FTcp_pair.count -arg i32  -write
+#FDb
 acr_ed -create -field x2bm_pcap.FDb.zd_tcp_pair -cascdel -write -comment ""
 
-# set packet
+#tcp pair
+acr_ed -create -ctype x2bm_pcap.FTcp_pair -pooltype Tpool -indexed -write  -comment "tcp pair entry"
+acr_ed -create -field x2bm_pcap.FTcp_pair.count -arg i32  -write
+acr_ed -create -field x2bm_pcap.FTcp_pair.zd_frames -arg x2bm_pcap.FFrame -via x2bm_pcap.FFrame.p_tcp_pair -cascdel -write -comment "L"
 
-acr_ed -create -ctype x2bm_pcap.FFrame -pooltype Tpool -write  -comment "tcp packet entry"
+#frame 
+acr_ed -create -ctype x2bm_pcap.FFrame -pooltype Tpool -write  -comment "tcp frame entry"
+acr_ed -create -field x2bm_pcap.FFrame.p_tcp_pair -arg x2bm_pcap.FTcp_pair -reftype Upptr -write
+
+acr_ed -create -field x2bm_pcap.FFrame.iframe    -arg u64                -write     -comment "global frame number"
 acr_ed -create -field x2bm_pcap.FFrame.seq   -arg u64                -write     -comment "sequence number"
+acr_ed -create -field x2bm_pcap.FFrame.ack   -arg u64                -write     -comment "ack      number"
 acr_ed -create -field x2bm_pcap.FFrame.p_buf -arg  u8  -reftype Tary -write     -comment "p to frame buffer "
+acr_ed -create -field x2bm_pcap.FFrame.th_flags -arg  u8             -write     -comment "tcp flas "
+
 
 
 
@@ -40,3 +48,5 @@ EOF
 
 amc 
 ai 
+
+echo "done!!!!!!!!!!!!"

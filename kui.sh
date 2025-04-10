8a05fe05f0fdf8a0fc4f184127a49dd0c4b74520
alexey vorovich <alexey.vorovich@gmail.com>
2025-04-10 17:07:02 -0400

    logs

--- kui.sh
+++ kui.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-omcli -omnode:dev.ak-8.uip -stop
-omcli -omnode:dev.ak-8.uip -start
-omcli -omnode:dev.ak-8.uip -status
+# omcli -omnode:dev.ak-8.uip -stop
+# omcli -omnode:dev.ak-8.uip -start
+# omcli -omnode:dev.ak-8.uip -status
+
+omcli -omnode:dev.ak-8.uir -stop
+omcli -omnode:dev.ak-8.uir -start
+omcli -omnode:dev.ak-8.uir -status
+
+# omcli -omnode:dev.ak-8.ui% -stop
+# omcli -omnode:dev.ak-8.ui% -start
+# omcli -omnode:dev.ak-8.ui% -status
 ofile=~/av_openacr/sniffer_logs/atf_snf.log
 sudo ~/arnd/bin/atf_snf -dev:data0-8 > $ofile 2>&1; tail -n 20 $ofile

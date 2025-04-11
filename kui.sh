7eaa81c451d19edd6c3d3a0d67886cd01ee0fa89
alexey vorovich <alexey.vorovich@gmail.com>
2025-04-11 16:48:11 -0400

    logs

--- kui.sh
+++ kui.sh
@@ -14,3 +14,5 @@ omcli -omnode:dev.ak-8.uir -status
 ofile=~/av_openacr/sniffer_logs/atf_snf.log
 export LIBIBVERBS_DEBUG=1
 sudo ~/arnd/bin/atf_snf -dev:data0-8  -out_file:o1.pcap > $ofile 2>&1; tail -n 20 $ofile
+exit
+sudo ~/arnd/bin/atf_snf -dev:data0-8   > $ofile 2>&1; tail -n 20 $ofile

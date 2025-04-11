2f88d09e53890d9eac44622bda5c2b3cb6785bdc
alexey vorovich <alexey.vorovich@gmail.com>
2025-04-10 22:21:14 -0400

    fix

--- kui.sh
+++ kui.sh
@@ -12,4 +12,6 @@ omcli -omnode:dev.ak-8.uir -status
 # omcli -omnode:dev.ak-8.ui% -start
 # omcli -omnode:dev.ak-8.ui% -status
 ofile=~/av_openacr/sniffer_logs/atf_snf.log
+export LIBIBVERBS_DEBUG=1
 sudo ~/arnd/bin/atf_snf -dev:data0-8 > $ofile 2>&1; tail -n 20 $ofile
+# sudo ~/arnd/bin/atf_snf -dev:data0 > $ofile 2>&1; tail -n 20 $ofile

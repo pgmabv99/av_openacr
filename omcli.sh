74fdec5eb94e94580733c1dfa829fb7396f61088
alexey vorovich <alexey.vorovich@gmail.com>
2025-05-20 16:03:41 -0400

    fix

--- omcli.sh
+++ omcli.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:hostname -q:Y -fail_on_error:Y
+echo "---------------------"
+omcli -omnode:dev.x2-4.x2% -start_clean -verbose
+echo "---------------------"
+omcli -omnode:dev.x2-4.x2% -status -verbose
+exit 0
+
+
+
 # omcli -omnode:dev.x2-4.x2-2 -dkr_clean_run -verbose
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2_ib  | hilite atf_ci.smokex2.*
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2 | hilite atf_ci.smokex2.*
 
 # x2rel -create -product:x2
-# x2rel -product:x2 -omnode:dev.x2-4.x2-1    -upload -dev
+# x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
 # x2rel -product:x2 -omnode:dev.x2-4.kafkaui-1  -upload -dev
 
 # omcli -omnode:dev.x2-4.kafkaui-1   -shell
@@ -14,7 +25,7 @@
 
 
 
-omcli -omnode:dev.x2-4.x2-1 -start_clean
+omcli -omnode:dev.x2-4.% -start_clean -verbose
 omcli -omnode:dev.x2-4.kafkaui-1   -start_clean
 
 omcli -omnode:dev.x2-4.%  -status
@@ -25,4 +36,3 @@ omcli -omnode:dev.x2-4.x2-1 -stop
 omcli -omnode:dev.x2-4.kafkaui-1   -stop
 
 # goto browser url
-

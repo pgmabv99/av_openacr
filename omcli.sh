8ca925c2123008238f3c304513fa8e61807fa932
alexey vorovich <alexey.vorovich@gmail.com>
2025-05-21 17:05:11 -0400

    logs

--- omcli.sh
+++ omcli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:hostname -q:N -fail_on_error:Y
-gdb --args bin/x2node -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:hostname -q:N -fail_on_error:Y
+bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:pwd -q:N -fail_on_error:Y
+bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:pwd  -fail_on_error:Y
+# gdb --args bin/x2node -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:hostname -q:N -fail_on_error:Y
 
 
 echo "---------------------"
@@ -15,8 +16,8 @@ exit 0
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2_ib  | hilite atf_ci.smokex2.*
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2 | hilite atf_ci.smokex2.*
 
-# x2rel -create -product:x2
-# x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
+x2rel -create -product:x2
+x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
 # x2rel -product:x2 -omnode:dev.x2-4.kafkaui-1  -upload -dev
 
 # omcli -omnode:dev.x2-4.kafkaui-1   -shell

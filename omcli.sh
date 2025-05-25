36cd004d1489fdf05d7eeb941edbaf8332929635
alexey vorovich <alexey.vorovich@gmail.com>
2025-05-25 19:09:50 -0400

    fix

--- omcli.sh
+++ omcli.sh
@@ -18,14 +18,31 @@ exit 0
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2_ib  | hilite atf_ci.smokex2.*
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2 | hilite atf_ci.smokex2.*
 
+# prod deployment on all nodes :source from x2-17
+down
+ai
+omcli -omnode:dev.x2-4.x2% -stop
 x2rel -create -product:x2
 x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload
-omcli -omnode:dev.x2-4.x2-1 -start_clean
+omcli -omnode:dev.x2-4.x2% -start_clean
+omcli -omnode:dev.x2-4.x2% -status
 
-x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
+# x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
 # x2rel -product:x2 -omnode:dev.x2-4.kafkaui-1  -upload -dev
 
-# omcli -omnode:dev.x2-4.kafkaui-1   -shell
+
+omcli -omnode:dev.x2-4.x2-1 -start_clean
+omcli -omnode:dev.x2-4.kafkaui-1   -stop
+omcli -omnode:dev.x2-4.kafkaui-1   -start_clean
+
+
+omcli -omnode:dev.ak-8.% -stop
+omcli -omnode:dev.ak-8.% -start
+# omcli -omnode:dev.ak-8.kafka-% -start
+omcli -omnode:dev.ak-8.kafka-% -start_clean
+omcli -omnode:dev.ak-8.kafka-% -status
+
+
 
 # # for i in 1 2 3 4; do
 # #   x2rel -product:x2 -omnode:dev.x2-4.x2-%    -upload -dev;
@@ -33,8 +50,5 @@ x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
 
 
 
-omcli -omnode:dev.x2-4.% -start_clean -verbose
-omcli -omnode:dev.x2-4.kafkaui-1   -stop
-omcli -omnode:dev.x2-4.kafkaui-1   -start_clean
 
 # goto browser url

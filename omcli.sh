9ce9e192501066dae3f91798dd19d07fba4d1676
alexey vorovich <alexey.vorovich@gmail.com>
2025-05-27 22:03:42 -0400

    fix

--- omcli.sh
+++ omcli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:pwd -q:N -fail_on_error:Y
 bin/x2node  -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:pwd  -fail_on_error:Y
+bin/x2node  -node:'dev.rdpui-5' -cmd:pwd  -fail_on_error:Y
 # gdb --args bin/x2node -node:'dev.x2-18|dev.x2-17|dev.x2-19' -cmd:hostname -q:N -fail_on_error:Y
 
 
@@ -41,7 +42,8 @@ omcli -omnode:dev.x2-4.kafkaui-1   -start_clean
 
 omcli -omnode:dev.x2-4.rdpui-1  -dkr_clean_run
 omcli -omnode:dev.x2-4.rdpui-1  -stop
-omcli -omnode:dev.x2-4.rdpui-1   -start_clean
+omcli -omnode:dev.x2-4.rdpui-1   -start
+bin/x2node  -node:'dev.rdpui-5' -cmd:'./rdpui_only start_clean'  -fail_on_error:Y
 
 
 omcli -omnode:dev.ak-8.% -stop

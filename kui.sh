527893af9d07fc0978c18a4eb306b2a1c7f76b7b
alexey vorovich <alexey.vorovich@gmail.com>
2025-06-05 19:45:46 -0400

    add wrk samples

--- kui.sh
+++ kui.sh
@@ -6,7 +6,7 @@ ofile=~/av_openacr/sniffer_logs/atf_snf.log
 echo "....starting atf_snf. use ctrl+C to enter commands . stdout is rediected to $ofile"
 echo "....monitoring subset in temp/atf_snf.dat . "
 tag=wrk
-sudo ~/arnd/bin/atf_snf -dev:data0-8 -kapi:true  -out_file:$tag.pcap  -out_solo_dir:$tag > $ofile 2>&1; tail -n 20 $ofile
+sudo ~/arnd/bin/atf_snf -dev:data0-8T -kapi:true  -out_file:$tag.pcap  -out_solo_dir:$tag > $ofile 2>&1; tail -n 20 $ofile
 
 
 

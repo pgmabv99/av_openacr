27a62cd1aa2db4b88f0384571b2cc330936a784f
alexey vorovich <alexey.vorovich@gmail.com>
2025-06-07 19:59:39 -0400

    logs

--- kui2.sh
+++ kui2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-tag=rdp
+tag=wrk
 ofile=~/av_openacr/sniffer_logs/atf_snf_$tag.log
 sudo chmod -R 777 ~/pcap/$tag
 atf_snf  -kapi:true  -in_file:$tag.pcap -out_solo_dir:$tag > $ofile 2>&1; tail -n 100 $ofile

23e3c0fac60f74e180c38626ba04f2e70c36f07c
alexey vorovich <alexey.vorovich@gmail.com>
2025-06-19 11:39:47 -0400

    fix omnode syntax

--- omcli.sh
+++ omcli.sh
@@ -6,78 +6,78 @@ bin/x2node  -node:'dev.rdpui-5' -cmd:pwd  -fail_on_error:Y
 
 
 echo "---------------------"
-omcli -omnode:dev.x2-4.x2-% -start_clean -verbose
+omcli dev.x2-4.x2-% -start_clean -verbose
 echo "---------------------"
-omcli -omnode:dev.x2-4.x2-% -status -verbose
+omcli dev.x2-4.x2-% -status -verbose
 
 
 exit 0
 
 
 
-# omcli -omnode:dev.x2-4.x2-2 -dkr_clean_run -verbose
+# omcli dev.x2-4.x2-2 -dkr_clean_run -verbose
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2_ib  | hilite atf_ci.smokex2.*
 #export OMENV=dev.x2-4 && atf_ci -citest:smokex2 | hilite atf_ci.smokex2.*
 
 # prod deployment on all nodes :source from x2-17
 down
 ai
-omcli -omnode:dev.x2-4.x2% -stop
+omcli dev.x2-4.x2% -stop
 x2rel -create  -product:x2
-x2rel  -upload -product:x2 -omnode:dev.x2-4.x2%
-omcli -omnode:dev.x2-4.x2% -start_clean
-omcli -omnode:dev.x2-4.x2% -status
+x2rel  -upload -product:x2 dev.x2-4.x2%
+omcli dev.x2-4.x2% -start_clean
+omcli dev.x2-4.x2% -status
 
 # sudo lsof -nP -iTCP -sTCP:LISTEN | grep x2gw
 
-# x2rel -product:x2 -omnode:dev.x2-4.x2%    -upload -dev
-# x2rel -product:x2 -omnode:dev.x2-4.kafkaui-1  -upload -dev
+# x2rel -product:x2 dev.x2-4.x2%    -upload -dev
+# x2rel -product:x2 dev.x2-4.kafkaui-1  -upload -dev
 
 
 echo "---------------------clean start ONE node + rdpui"
-omcli -omnode:dev.x2-4.% -stop
-omcli -omnode:dev.x2-4.x2-0 -start_clean
+omcli dev.x2-4.% -stop
+omcli dev.x2-4.x2-0 -start_clean
 bin/x2node  -node:dev.rdpui-5   -cmd:'./rdpui_only start_clean'    -fail_on_error:Y
 bin/x2node  -node:dev.kafkaui-5 -cmd:'./kafkaui_only start_clean'  -fail_on_error:Y
 echo "---------------------status"
-omcli -omnode:dev.x2-4.x2-0  -status
-omcli -omnode:dev.x2-4.rdpui-1 -status 
-omcli -omnode:dev.x2-4.kafkaui-1 -status 
+omcli dev.x2-4.x2-0  -status
+omcli dev.x2-4.rdpui-1 -status 
+omcli dev.x2-4.kafkaui-1 -status 
 
 echo "---------------------clean start ALL  node + rdpui"
-omcli -omnode:dev.x2-4.% -stop
-omcli -omnode:dev.x2-4.x2-% -start_clean
-omcli -omnode:dev.x2-4.rdpui-1 -start_clean
+omcli dev.x2-4.% -stop
+omcli dev.x2-4.x2-% -start_clean
+omcli dev.x2-4.rdpui-1 -start_clean
 echo "---------------------status"
-omcli -omnode:dev.x2-4.x2-% -status 
-omcli -omnode:dev.x2-4.rdpui-1 -status 
+omcli dev.x2-4.x2-% -status 
+omcli dev.x2-4.rdpui-1 -status 
 
 
 
-omcli -omnode:dev.x2-4.kafkaui-1   -dkr_clean_run
-omcli -omnode:dev.x2-4.rdpui-1     -dkr_clean_run
+omcli dev.x2-4.kafkaui-1   -dkr_clean_run
+omcli dev.x2-4.rdpui-1     -dkr_clean_run
 
 # bin/x2node  -node:dev.rdpui-5 -shell
 
 #          ===============
-omcli -omnode:dev.x2-4.kafkaui-1  -stop
+omcli dev.x2-4.kafkaui-1  -stop
 bin/x2node  -node:dev.kafkaui-5 -cmd:'./kafkaui_only start_clean'  -fail_on_error:Y
 
-omcli -omnode:dev.x2-4.kafkaui-1  -status
+omcli dev.x2-4.kafkaui-1  -status
 
 
 
-omcli -omnode:dev.ak-8.% -stop
-omcli -omnode:dev.ak-8.% -start
-# omcli -omnode:dev.ak-8.kafka-% -start
-omcli -omnode:dev.ak-8.kafka-% -start_clean
-omcli -omnode:dev.ak-8.kafka-% -status
+omcli dev.ak-8.% -stop
+omcli dev.ak-8.% -start
+# omcli dev.ak-8.kafka-% -start
+omcli dev.ak-8.kafka-% -start_clean
+omcli dev.ak-8.kafka-% -status
 
 
 
 # # for i in 1 2 3 4; do
-# #   x2rel -product:x2 -omnode:dev.x2-4.x2-%    -upload -dev;
-# # doneomcli -omnode:dev.x2-4.x2-% -start_clean -verbose
+# #   x2rel -product:x2 dev.x2-4.x2-%    -upload -dev;
+# # doneomcli dev.x2-4.x2-% -start_clean -verbose
 
 
 

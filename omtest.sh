def8da1de33c2af31ccf18f45c2f53f775ec935b
alexey vorovich <alexey.vorovich@gmail.com>
2025-08-14 11:27:10 -0400

    fixes

--- omtest.sh
+++ omtest.sh
@@ -1,1 +1,6 @@
-bin/omcli  -selector:dev.x2-4 -omtest:omtest_x2_omcli_options-ci:Y -omrun_minutes:1
+bin/omcli  -selector:dev.x2-4 -omtest:x2_omcli_options -ci:Y -omrun_minutes:1
+bin/omcli  -selector:dev.x2-4 -omtest:x2_kcat_100b_1 -ci:Y -omrun_minutes:1
+
+bin/omcli  -selector:dev.x2-4 -omtest:% -ci:Y -omrun_minutes:1
+
+atf_ci -cijob:envx2 -check_clean:N -v
